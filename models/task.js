6ca22c6d34bd94af79810ac273701f86e23a178d
Jed <kohjedrek@yahoo.com.sg>
2024-06-09 12:16:23 +0800

    Updated controllers and model

---
+++ models/task.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const TaskSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Must provide Name"],
+    trim: true,
+    maxlength: [20, " Name cannot be more than 20"],
+  },
+  completed: { type: Boolean, default: false },
+});
+
+module.exports = mongoose.model("Task", TaskSchema);
